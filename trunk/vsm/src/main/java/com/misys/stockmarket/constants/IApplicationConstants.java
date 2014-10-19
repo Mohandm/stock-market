c7c20ba45e939a0260b1185b7c261f6a2311f055
bipinpdl@gmail.com <bipinpdl@gmail.com@fecf67dc-d740-a123-937e-cb981158c489>
2014-10-19 07:08:38 +0000

    Leagues and install default leagues

--- trunk/vsm/src/main/java/com/misys/stockmarket/constants/IApplicationConstants.java
+++ trunk/vsm/src/main/java/com/misys/stockmarket/constants/IApplicationConstants.java
@@ -1,5 +1,7 @@
 package com.misys.stockmarket.constants;
 
+import java.math.BigDecimal;
+
 public interface IApplicationConstants {
 
 	public final String EMAIL_VERIFIED_YES = "Y";
@@ -15,22 +17,46 @@ public interface IApplicationConstants {
 	public final String STOCK_ACTIVE = "A";
 
 	public final String STOCK_INACTIVE = "I";
-	
+
 	public final String BUY_TYPE = "B";
-	
+
 	public final String SELL_TYPE = "S";
-	
+
 	public final String ORDER_PRICE_TYPE_MARKET = "01";
-	
+
 	public final String ORDER_PRICE_TYPE_LIMIT = "02";
-	
+
 	public final String ORDER_PRICE_TYPE_STOPLOSS = "03";
-	
+
 	public final String ORDER_INTRADAY_YES = "Y";
-	
+
 	public final String ORDER_INTRADAY_NO = "N";
-	
+
 	public final String ORDER_STATUS_PENDING = "P";
 
 	public final String ORDER_STATUS_COMPLETED = "C";
+
+	public final String LEAGUE_DURATION_DAILY = "D";
+
+	public final String LEAGUE_DURATION_WEEKLY = "W";
+
+	public final String LEAGUE_DURATION_FORTNIGHTLY = "F";
+
+	public final String LEAGUE_DURATION_MONTHLY = "M";
+
+	public final String LEAGUE_DURATION_YEARLY = "Y";
+
+	public final String LEAGUE_STATUS_ACTIVE = "A";
+
+	public final String LEAGUE_STATUS_INACTIVE = "I";
+
+	public final String LEAGUE_STATUS_COMPLETED = "C";
+
+	public final String DEFAULT_LEAGUE_NAME = "Global";
+
+	public final BigDecimal DEFAULT_LEAGUE_START_AMOUNT = BigDecimal
+			.valueOf(1000000);
+
+	public final BigDecimal DEFAULT_LEAGUE_QUALIFYING_AMOUNT = BigDecimal
+			.valueOf(750000);
 }
