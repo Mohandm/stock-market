748b050e53aae59cd75aefb4f6b7fc0866ddb952
gurudath.jcreddy@gmail.com <gurudath.jcreddy@gmail.com@fecf67dc-d740-a123-937e-cb981158c489>
2014-10-25 15:29:45 +0000

    The Final Game

--- trunk/vsm/src/main/webapp/app/scripts/services/leaguesService.js
+++ trunk/vsm/src/main/webapp/app/scripts/services/leaguesService.js
@@ -2,6 +2,18 @@ var vsmApp = angular.module('vsmApp');
 
 vsmApp.service('LeaguesService', ['$http','$q','$log', function ($http, $q, $log) {
 
-
+    this.getGameLeagues = function(){
+        var deferred = $q.defer();
+        //var actionUrl = 'gameLeaguesList/';
+        var actionUrl = 'app/data/my_leagues.json';
+        $http.post(actionUrl,{})
+            .success(function (json) {
+                deferred.resolve(json);
+            }).error(function(msg, code) {
+                deferred.reject(msg);
+                $log.error(msg, code);
+            });
+        return deferred.promise;
+    };
 
 }]);
