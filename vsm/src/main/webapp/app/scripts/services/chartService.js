4b2f41f8c863cadc4d49bd9a55c19868e8cc6b56
gurudath.jcreddy@gmail.com <gurudath.jcreddy@gmail.com@fecf67dc-d740-a123-937e-cb981158c489>
2014-10-25 07:56:26 +0000

    Charts Issues

--- vsm/src/main/webapp/app/scripts/services/chartService.js
+++ vsm/src/main/webapp/app/scripts/services/chartService.js
@@ -1,17 +1,22 @@
 var vsmApp = angular.module('vsmApp');
 
-vsmApp.service('ChartService', ['$http', function ($http) {
+vsmApp.service('ChartService', ['$http','StockQuotesService', function ($http,StockQuotesService) {
 
     this.reloadSparklingCharts = function(symbol, historicalData, quoteData){
-
-        var chartData = [];
-        $(historicalData).each(function(index, item){
-            chartData.push(item.close);
+        var reducedData1 = StockQuotesService.reducedHistoricalData(historicalData, 50);
+        var reducedData2 = StockQuotesService.reducedHistoricalData(historicalData, 42);
+        var chartData1 = [];
+        var chartData2 = [];
+        $(reducedData1).each(function(index, item){
+            chartData1.push(item.close);
+        });
+        $(reducedData2).each(function(index, item){
+            chartData2.push(item.close);
         });
         //chartData = chartData.reverse();
-        $("#stats-line").sparkline(chartData, {
+        $("#stats-line").sparkline(chartData1, {
             type: 'line',
-            width: '300',
+            width: '250',
             height: '150',
             lineColor: 'rgba(255,255,255,0.4)',
             fillColor: 'rgba(0,0,0,0.2)',
@@ -20,9 +25,9 @@ vsmApp.service('ChartService', ['$http', function ($http) {
             barColor: '#C5CED6'
         });
 
-        $("#stats-line-2").sparkline(chartData, {
+        $("#stats-line-2").sparkline(chartData2, {
             type: 'bar',
-            width: '300',
+            width: '250',
             height: '150',
             lineColor: 'rgba(255,255,255,0.4)',
             fillColor: 'rgba(0,0,0,0.2)',
@@ -31,9 +36,9 @@ vsmApp.service('ChartService', ['$http', function ($http) {
             barColor: '#C5CED6'
         });
 
-        $("#stats-line-3").sparkline(chartData, {
+        $("#stats-line-3").sparkline(chartData1, {
             type: 'discrete',
-            width: '300',
+            width: '250',
             height: '150',
             lineColor: 'rgba(255,255,255,0.4)',
             lineWidth: 1.25,
